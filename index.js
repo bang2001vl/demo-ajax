e7e9ae973abc42f3c1ca7df280a3f625d52a2056
Dragnell <bang2001vl@outlook.com.vn>
2022-03-27 21:28:57 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const { readFileSync } = require("fs");
+
+const app = express();
+
+app.get("/", (req, res, next)=>{
+    res.sendFile(__dirname + "/routes/index.html");
+});
+
+app.get("/afterClick", (req, res, next)=>{
+    res.sendFile(__dirname + "/routes/afterClick.html");
+});
+
+// app.get("/ajax/afterClick", (req, res, next)=>{
+//     const xml = readFileSync(__dirname + "/routes/afterClick-ajax.xml")
+//     res.send(xml);
+// });
+
+app.listen(8080, ()=>{
+    console.log("Listening on port 8080");
+});
