19652647ce1d7289c2eed0e7f0e61bf4629529b4
Dragnell Nguyễn <bang2001vl@outlook.com.vn>
2022-10-22 18:03:19 +0700

    update full

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ app.get("/afterClick", (req, res, next)=>{
     res.sendFile(__dirname + "/routes/afterClick.html");
 });
 
-// app.get("/ajax/afterClick", (req, res, next)=>{
-//     const xml = readFileSync(__dirname + "/routes/afterClick-ajax.xml")
-//     res.send(xml);
-// });
+app.get("/ajax/afterClick", (req, res, next)=>{
+    const xml = readFileSync(__dirname + "/routes/afterClick-ajax.xml")
+    res.send(xml);
+});
 
 app.listen(8080, ()=>{
     console.log("Listening on port 8080");
